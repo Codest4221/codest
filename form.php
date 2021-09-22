b251f11a7133010d31e444a153664c846c88f4ca
pinartalas <pinartalas@hotmail.com.tr>
2021-09-22 14:06:08 +0300

    aa

--- form.php
+++ form.php
@@ -15,7 +15,7 @@ $email_body = "User Name: $name.\n".
 
 $to ='codest.4221@gmail.com';
 $headers = "From: $email_from \r\n";
-$headers .= "Reply-To :visito_email \r\n";
+$headers .= "Reply-To :visitor_email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 header("Location: contact.html");
