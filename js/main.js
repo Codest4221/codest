8f2491433ad2412b13caf3e664e17f2831094662
pinartalas <codest.4221@gmail.com>
2021-12-05 03:46:23 +0300

    ending

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+const navSlide = () => {
+    const burger =document.querySelector('.burger');
+    const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.nav li');
+   
+
+burger.addEventListener('click',()=>{
+   nav.classList.toggle('nav-active');
+
+   navLinks.forEach((link,index) => {
+       if (link.style.animation) {
+           link.style.animation = '';
+
+       } else {
+           link.style.animation ='navLinkFade 0.5s ease forwards  ${index / 7 + 0.3}s';
+       }
+   });
+
+
+
+
+
+
+        burger.classList.toggle('toggle');
+    });
+
+
+  
+}
+ navSlide();
+
+
+
+
 const responsive = {
     0: {
         items:1
